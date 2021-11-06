d8c967ec96dde9210cf573633746adf2dc3ea048
Andras Gyarmati <bandieee16@gmail.com>
2021-11-06 23:47:39 +0100

    refactor wip

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const GameStates = Object.freeze({
 });
 
 class Cell {
-    constructor(code) {
+    constructor(code, rowIndex, colIndex) {
         this.sides = code.split("");
+        this.cellEm = createCellEm(rowIndex, colIndex);
     }
 }
 
@@ -36,12 +37,11 @@ const initialGridData = [
 ];
 let playerCount = 1; // todo: from user input
 let treasureCount = 1; // todo: from user input
-
-let initExtraCellCode = "0110";
-let extraCell = new Cell(initExtraCellCode);
-
+// amikor a nyil fole megyunk akkor beallitjuk oda az extranak a pozijat ha lemegyunk rola akkor meg visszaallitjuk arra a helyre ahol az extranak
+// lennie kell, csak a belsok legyenek rendes jatek cellak, a nyilasokra mas eventlistener kene
+// legyen szepen elkulonitve minden
+let extraCell = new Cell("0110", -1, -1);
 let gridData = [];
-
 let players = [];
 
 function genCells() {
@@ -61,6 +61,10 @@ function isFixed(x, y) {
     return x % 2 == 1 && y % 2 == 1;
 }
 
+function moveCell(rowIndex, colIndex) {
+    setCellEmPos(gridData[rowIndex][colIndex].cellEm, rowIndex, colIndex);
+}
+
 function cellClicked(event) {
     let cell = event.target;
     let colIndex = cell.dataset.colIndex;
@@ -70,8 +74,10 @@ function cellClicked(event) {
         tmpCell = gridData[gridSize - 1][colIndex];
         for (let i = gridSize - 1; i > 0; i--) {
             gridData[i][colIndex] = gridData[i - 1][colIndex];
+            moveCell(i, colIndex);
         }
         gridData[0][colIndex] = extraCell;
+        moveCell(0, colIndex);
         extraCell = tmpCell;
     } else if (rowIndex == gridSize) {
         tmpCell = gridData[0][colIndex];
@@ -95,77 +101,83 @@ function cellClicked(event) {
         gridData[rowIndex][gridSize - 1] = extraCell;
         extraCell = tmpCell;
     }
-    drawGrid();
 }
 
 function onArrowCellMouseEnter(el, cell) {
     if (cell === null) cell = extraCell;
-    el.style.borderStyle = cell.sides
-        .map((x) => (x === "1" ? "none" : "solid"))
-        .join(" ");
+    el.style.borderStyle = cell.sides.map((x) => (x === "1" ? "none" : "solid")).join(" ");
     el.style.borderWidth = "15px";
     el.style.borderWidth = "15px";
     el.classList.remove("arrow-cell");
-    el.classList.remove("border-cell");
     el.classList.add("cell");
 }
 
 function onArrowCellMouseLeave(el) {
     el.style.borderStyle = "";
     el.classList.add("arrow-cell");
-    el.classList.add("border-cell");
-    el.classList.remove("cell");
 }
 
 // todo: add element to the cell object and move them around when cells are moved in the underlaying griddata structure
+// extracellt is kezelni kell cellEm-el egyutt meg minden, nem kellenek bordercellek csak arrowcellek
+
+function setCellEmPos(cellEm, rowIndex, colIndex) {
+    console.log(`${rowIndex} ${colIndex}`);
+    cellEm.dataset.rowIndex = rowIndex;
+    cellEm.dataset.colIndex = colIndex;
+    cellEm.style.top = `${(parseInt(rowIndex) + 1) * cellSize}px`;
+    cellEm.style.left = `${(parseInt(colIndex) + 1) * cellSize}px`;
+}
+
+function createCellEm(rowIndex, colIndex) {
+    let cellEm = document.createElement("div");
+    cellEm.classList.add("cell");
+    cellEm.style.position = "absolute";
+    setCellEmPos(cellEm, rowIndex, colIndex);
+    cellEm.addEventListener("click", cellClicked);
+    return cellEm;
+}
+
+function isGridCell(rowIndex, colIndex) {
+    return 0 <= rowIndex && rowIndex < gridSize && 0 <= colIndex && colIndex < gridSize;
+}
+
+function setupGridCell(rowIndex, colIndex, cellEm) {
+    cellEm.classList.add("grid-cell");
+    cellEm.classList.add("animated-cell");
+    let cell = gridData[rowIndex][colIndex];
+    cellEm.style.borderStyle = cell.sides.map((x) => (x === "1" ? "none" : "solid")).join(" ");
+    cellEm.style.borderWidth = "15px";
+    cell.cellEm = cellEm;
+}
+function isArrowCell(rowIndex, colIndex) {
+    return (
+        Math.abs(rowIndex % 2) === 1 &&
+        Math.abs(colIndex % 2) === 1 &&
+        ((rowIndex !== -1 && rowIndex !== gridSize) || (colIndex !== -1 && colIndex !== gridSize))
+    );
+}
+function setupArrowCells(cellEm) {
+    cellEm.classList.add("arrow-cell");
+    cellEm.addEventListener("mouseenter", () => onArrowCellMouseEnter(cellEm, null));
+    cellEm.addEventListener("mouseleave", () => onArrowCellMouseLeave(cellEm));
+    cellEm.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+        rotateCell(extraCell);
+        onArrowCellMouseEnter(cellEm, null);
+    });
+}
 
 function drawGrid() {
     grid.innerHTML = "";
     for (let rowIndex = -1; rowIndex <= gridSize; rowIndex++) {
         for (let colIndex = -1; colIndex <= gridSize; colIndex++) {
-            let cellEm = document.createElement("div");
-            cellEm.classList.add("cell");
-            cellEm.dataset.rowIndex = rowIndex;
-            cellEm.dataset.colIndex = colIndex;
-            cellEm.style.position = "absolute"
-            cellEm.style.top = `${(rowIndex + 1) * cellSize}px`
-            cellEm.style.left = `${(colIndex + 1) * cellSize}px`
-            if (
-                0 <= rowIndex &&
-                rowIndex < gridSize &&
-                0 <= colIndex &&
-                colIndex < gridSize
-            ) {
-                let cell = gridData[rowIndex][colIndex];
-                cellEm.style.borderStyle = cell.sides
-                    .map((x) => (x === "1" ? "none" : "solid"))
-                    .join(" ");
-                cellEm.style.borderWidth = "15px";
+            if (!isArrowCell(rowIndex, colIndex) && !isGridCell(rowIndex, colIndex)) continue;
+            let cellEm = gridData[rowIndex][colIndex].cellEm;
+            if (isGridCell(rowIndex, colIndex)) {
+                setupGridCell(rowIndex, colIndex, cellEm);
             } else {
-                cellEm.classList.add("border-cell");
-                if (
-                    Math.abs(rowIndex % 2) === 1 &&
-                    Math.abs(colIndex % 2) === 1 &&
-                    ((rowIndex !== -1 && rowIndex !== gridSize) ||
-                        (colIndex !== -1 && colIndex !== gridSize))
-                ) {
-                    cellEm.classList.add("arrow-cell");
-                    cellEm.addEventListener("mouseenter", () =>
-                        onArrowCellMouseEnter(cellEm, null)
-                    );
-                    cellEm.addEventListener("mouseleave", () =>
-                        onArrowCellMouseLeave(cellEm)
-                    );
-                    cellEm.addEventListener("contextmenu", (e) => {
-                        e.preventDefault();
-                        rotateCell(extraCell);
-                        onArrowCellMouseEnter(cellEm, null);
-                    });
-                }
+                setupArrowCells(cellEm);
             }
-
-            cellEm.addEventListener("click", cellClicked);
-
             grid.appendChild(cellEm);
         }
     }
